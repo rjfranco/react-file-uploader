ae1f503a465f49c1dfbda4109400dfbe8024a184
Ramiro Jr. Franco <rjfranco@gmail.com>
2018-08-10 17:38:10 -0700

    Adding test to validate state is set in app when function is called

--- src/App.test.js
+++ src/App.test.js
@@ -5,3 +5,11 @@ import { shallow } from 'enzyme';
 it('renders without crashing', () => {
   shallow(<App />);
 });
+
+describe('#onFileInfoChange', () => {
+  it('should set this data to the state', () => {
+    const app = shallow(<App />);
+    app.instance().onFileInfoChange({test: 'value'});
+    expect(app.state('fileInfo').test).toEqual('value');
+  });
+});
